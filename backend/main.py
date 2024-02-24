08e2d3ee7720d8bb1c8aaba46f0a17e1458b4811
nar2p1nk <farrisisqandar@gmail.com>
2024-02-25 03:41:32 +0800

    flask-sqlalchemy initialised

--- backend/main.py
+++ backend/main.py
@@ -1,36 +1,41 @@
 from flask import Flask,jsonify,request
-import sqlalchemy as db
-from dotenv import load_dotenv
 import os
-
-
-# loading the database engine
-load_dotenv()
-
-engine = db.create_engine(os.getenv('POSTGRES_ENGINE'))
-
-conn = engine.connect()
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 
 
 app = Flask(__name__)
 
+users = ['placeholder]']
+
+# initialize flask-sqlalchemy
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('POSTGRES_ENGINE')
 
 
 @app.route('/')
 def main():
-    return jsonify({'server status':'running'})
+    return jsonify({'server status': 'running'})
+
 
 @app.route('/users/', methods=['GET'])
 def getUsers():
 
-    return jsonify({'users':users})
+    return jsonify({'users': users})
 
 
-
-@app.route('/users/create',methods=['POST'])
+@app.route('/users/create', methods=['POST'])
 def createUser():
 
-    return jsonify({"users":users})
+    return jsonify({"users": users})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
