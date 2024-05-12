63ef9cebf0ee549da011ac60a581c7c4272b4c29
nar2p1nk <farrisisqandar@gmail.com>
2024-05-13 03:40:48 +0800

    got a simple setup in typescript

---
+++ backend/main.ts
@@ -0,0 +1,16 @@
+const express:Function = require('express')
+
+const app = express()
+
+console.log(typeof app)
+
+const port:Number = 8080
+
+app.get('/',(req:any, res:any)=>{
+    res.json({"status":"Hello World"})
+})
+
+app.listen(port,()=>{
+    console.log(`app listening on port ${port}`);
+    
+})
