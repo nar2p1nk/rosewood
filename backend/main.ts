84a44b773dd1192261d1111239d0cee5745215bd
nar2p1nk <farrisisqandar@gmail.com>
2024-05-15 04:05:54 +0800

    split express into two to run server while testing app

--- backend/main.ts
+++ backend/main.ts
@@ -1,16 +1,9 @@
-import express from 'express'
+import * as express from 'express'
 
-const app = express()
-
-console.log(typeof app)
-
-const port:Number = 8080
+const app:any = express()
 
 app.get('/',(req:any, res:any)=>{
-    res.json({"status":"Hello World"})
+    res.status(200).json({"status":"Hello World"})
 })
 
-app.listen(port,()=>{
-    console.log(`app listening on port ${port}`);
-    
-})
+export default app;
---
+++ backend/server.ts
@@ -0,0 +1,8 @@
+import app from './main'
+
+const port:Number = 8080;
+
+app.listen(port,()=>{
+    console.log(`app listening on port ${port}`);
+    
+})
